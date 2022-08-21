198ea105044384ee37190feac1e5972d39220146
aaquibkuwari <aaquib.kuwari@gmail.com>
2022-08-21 21:01:23 +0530

    php converted to html

--- common-pages/footer.php
+++ common-pages/footer.php
@@ -14,8 +14,7 @@
                  <div class="col-lg-3 col-md-6">
                      <div class="mb-5 border-left pl-3">
                          <span class="d-block mb-3">Send us email</span>
-                         <div class="h6 text-white"><a class="clr" href="mailto:alazratraders@gmail.com"
-                                 target="_blank">alazratraders@gmail.com</a>
+                         <div class="h6 text-white"><a class="clr" href="mailto:alazratraders@gmail.com" target="_blank">alazratraders@gmail.com</a>
                          </div>
                          <span class="text-primary">Response time avg 24/h</span>
                      </div>
@@ -23,8 +22,7 @@
                  <div class="col-lg-6 col-md-12">
                      <div class="mb-5 border-left pl-3">
                          <span class="d-block mb-3">Visit in office</span>
-                         <div class="h6 text-white"><a class="clr" href="https://g.page/al-azra-traders-bhiwandi?share"
-                                 target="_blank">157,
+                         <div class="h6 text-white"><a class="clr" href="https://g.page/al-azra-traders-bhiwandi?share" target="_blank">157,
                                  Prabhu Aley, Gala No:-4, Naya Bazar Bldg Near BMS Hospital, Khumbhar,
                                  MIDC, Bhiwandi, Maharashtra 421302</a></div>
                          <span class="text-primary">10:00am - 9:30pm week</span>
@@ -32,20 +30,13 @@
                  </div>
              </div>
              <div class=" social">
-                 <a href="mailto:alazratraders@gmail.com" target="_blank"><i
-                         class="style fa-duotone fa-brands fa-google-plus"></i></a>
-                 <a href="https://api.whatsapp.com/send?phone=+917021223634&amp;text=I%20want%20to%20find%20out%20about%20your%20courses"
-                     target="_blank"><i class="fa-duotone fa-brands fa-whatsapp"></i></a>
-                 <a href="https://www.instagram.com/alazratraders/" target="_blank"><i
-                         class="fa-duotone fa-brands fa-instagram"></i></a>
-                 <a href="https://www.facebook.com/alazratraders01/" target="_blank"><i
-                         class="fa-duotone fa-brands fa-facebook"></i></a>
-                 <a href="https://www.linkedin.com/in/alazratraders" target="_blank"><i
-                         class="fa-duotone fa-brands fa-linkedin"></i></a>
-                 <a href="https://twitter.com/Alazratraders" target="_blank"><i
-                         class="fa-duotone fa-brands fa-twitter"></i></a>
-                 <a href="https://in.pinterest.com/alazratraders/" target="_blank"><i
-                         class="fa-duotone fa-brands fa-pinterest"></i></a>
+                 <a href="mailto:alazratraders@gmail.com" target="_blank"><i class="style fa-duotone fa-brands fa-google-plus"></i></a>
+                 <a href="https://api.whatsapp.com/send?phone=+917021223634&amp;text=I%20want%20to%20find%20out%20about%20your%20courses" target="_blank"><i class="fa-duotone fa-brands fa-whatsapp"></i></a>
+                 <a href="https://www.instagram.com/alazratraders/" target="_blank"><i class="fa-duotone fa-brands fa-instagram"></i></a>
+                 <a href="https://www.facebook.com/alazratraders01/" target="_blank"><i class="fa-duotone fa-brands fa-facebook"></i></a>
+                 <a href="https://www.linkedin.com/in/alazratraders" target="_blank"><i class="fa-duotone fa-brands fa-linkedin"></i></a>
+                 <a href="https://twitter.com/Alazratraders" target="_blank"><i class="fa-duotone fa-brands fa-twitter"></i></a>
+                 <a href="https://in.pinterest.com/alazratraders/" target="_blank"><i class="fa-duotone fa-brands fa-pinterest"></i></a>
              </div>
          </div>
      </footer>
--- common-pages/header.php
+++ common-pages/header.php
@@ -5,31 +5,29 @@
       <div class="row">
         <div class="col-md-12">
           <nav class="navbar navbar-expand-lg nav-white nav-primary-hover">
-            <a class="navbar-brand d-none" href="#"><img class="nav-logo" src="assets/images/logo/10.png"
-                alt="Image not found !"></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <a class="navbar-brand d-none" href="#"><img class="nav-logo" src="assets/images/logo/10.png" alt="Image not found !"></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon flaticon-menu flat-small text-white"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav">
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="index.php">Home</a>
+                  <a class="nav-link dropdown-toggle" href="index.html">Home</a>
 
                 </li>
-                <li class="nav-item"><a class="nav-link" href="about.php">About</a> </li>
-                <li class="nav-item"><a class="nav-link" href="service.php">Service</a></li>
+                <li class="nav-item"><a class="nav-link" href="about.html">About</a> </li>
+                <li class="nav-item"><a class="nav-link" href="service.html">Service</a></li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="gallery.php">Products</a>
+                  <a class="nav-link dropdown-toggle" href="gallery.html">Products</a>
                   <ul class="dropdown-menu">
-                    <li><a class="dropdown-item" href="gallery.php">Classic Gallery</a></li>
-                    <li><a class="dropdown-item" href="gallery-simple.php">Simple Gallery</a></li>
+                    <li><a class="dropdown-item" href="gallery.html">Classic Gallery</a></li>
+                    <li><a class="dropdown-item" href="gallery-simple.html">Simple Gallery</a></li>
                   </ul>
                 </li>
                 <li class="nav-item dropdown">
-                  <a class="nav-link dropdown-toggle" href="company-profile.php">Company Profile</a>
+                  <a class="nav-link dropdown-toggle" href="company-profile.html">Company Profile</a>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
               </ul>
             </div>
           </nav>
